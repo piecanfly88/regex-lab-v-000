53e82afb33156caaeb53f06f5626d50d1ac7f878
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2018-04-13 23:04:47 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,9 @@
 def starts_with_a_vowel?(word)
-
+  !!(word.match(/\b[aeiou]/i))
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(\Aun(?:\s*\S+)ing\Z)
 end
 
 def words_five_letters_long(text)
