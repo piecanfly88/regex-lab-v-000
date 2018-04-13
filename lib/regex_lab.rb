81c42f7ae79fc6a8c3a6f5ef0ba794695f4bb054
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2018-04-13 23:06:45 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,7 +3,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/\Aun(?:\s*\S+)ing\Z/).count
+  text.scan(/\Aun(?:\s*\S+)ing\Z/).length
 end
 
 def words_five_letters_long(text)
