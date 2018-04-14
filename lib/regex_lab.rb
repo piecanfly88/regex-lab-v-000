8acb98f4fb39332a1e97263cd4ba4c95bdd2b149
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2018-04-14 00:01:32 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -6,7 +6,8 @@ def words_starting_with_un_and_ending_with_ing(text)
   text.scan(/un\w*ing\b/i)
 end
 
-def words_five_letters_long(text)
+def words_five_letters_
+  long(text)
   text.scan(/.{3}\w\s\b/i)
 end
 
